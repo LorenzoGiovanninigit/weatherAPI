75c20a6682310a59dbf9e23382911bd5096acab4
LorenzoGiovanninigit <lorenzogiovannini19@gmail.com>
2021-05-04 11:54:08 +0200

    simple weather app using openWeather

---
+++ app.js
@@ -0,0 +1,23 @@
+//selctor and variables
+const cont = document.getElementById("container");
+const city = document.getElementById("cityInput");
+const sub = document.getElementById("btn");
+const display = document.getElementById("box-temp");
+const weather = document.getElementById("box-weather");
+
+//get meteo function
+async function getMeteo() {
+  let cityInput = city.value;
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=312ec4308b49eb2608b97eb87fc8f9c6`
+  );
+  const fettched = await response.json();
+  const desc = fettched.weather[0].description;
+  const temps = fettched.main.feels_like;
+  display.textContent = `${temps} C`;
+  weather.textContent = `${desc}`;
+}
+
+//calling the function on submit
+
+sub.onclick = getMeteo;
